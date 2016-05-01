8052451312be09b66150c6cefa27411b4c20cfbb
Connor Leech <connorleech@gmail.com>
2016-05-01 16:54:30 -0700

    do some things

---
+++ routes/auth/confirm.js
@@ -0,0 +1,163 @@
+var keystone = require('keystone'),
+	async = require('async'),
+	request = require('request'),
+	_ = require('lodash'),
+	User = keystone.list('User');
+
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res);
+
+	console.log('req.user: ' + req.user);
+	
+	// // Function to handle signin
+	// var doSignIn = function() {
+	
+	// 	console.log('[auth.confirm] - Signing in user...');
+	// 	console.log('------------------------------------------------------------');
+		
+	// 	var onSuccess = function(user) {
+	// 		console.log('[auth.confirm] - Successfully signed in.');
+	// 		console.log('------------------------------------------------------------');
+	// 		return res.redirect('/profile');
+	// 	}
+		
+	// 	var onFail = function(err) {
+	// 		console.log('[auth.confirm] - Failed signing in.', err);
+	// 		console.log('------------------------------------------------------------');
+	// 		req.flash('error', 'Sorry, there was an issue signing you in, please try again.');
+	// 		return res.redirect('/');
+	// 	}
+		
+	// 	keystone.session.signin(String(locals.existingUser._id), req, res, onSuccess, onFail);
+	
+	// }
+	
+	// // Function to handle data confirmation process
+	// var checkAuth = function() {
+	
+	// 	async.series([
+			
+	// 		// Create or update user
+	// 		function(next) {
+			
+				
+	// 				console.log('[auth.confirm] - Existing user found, updating...');
+	// 				console.log('------------------------------------------------------------');
+					
+					
+	// 				_.extend(userData.services[locals.authUser.type], {
+	// 					isConfigured: true,
+						
+	// 					profileId: locals.authUser.profileId,
+						
+	// 					username: locals.authUser.username,
+	// 					avatar: locals.authUser.avatar,
+						
+	// 					accessToken: locals.authUser.accessToken,
+	// 					refreshToken: locals.authUser.refreshToken
+	// 				});
+					
+					
+	// 				locals.existingUser.set(userData);
+					
+	// 				locals.existingUser.save(function(err) {
+	// 					if (err) {
+	// 						console.log('[auth.confirm] - Error saving existing user.', err);
+	// 						console.log('------------------------------------------------------------');
+	// 						return next({ message: 'Sorry, there was an error processing your account, please try again.' });
+	// 					}
+	// 					console.log('[auth.confirm] - Saved existing user.');
+	// 					console.log('------------------------------------------------------------');
+	// 					return next();
+	// 				});
+				
+	// 			} else {
+				
+	// 				console.log('[auth.confirm] - Creating new user...');
+	// 				console.log('------------------------------------------------------------');
+					
+	// 				var userData = {
+	// 					name: {
+	// 						first: locals.form['name.first'],
+	// 						last: locals.form['name.last']
+	// 					},
+	// 					email: locals.form.email,
+	// 					password: Math.random().toString(36).slice(-8),
+						
+	// 					state: 'enabled',
+						
+	// 					website: locals.form.website,
+						
+	// 					isVerified: true,
+						
+	// 					services: {}
+	// 				};
+					
+	// 				userData.services[locals.authUser.type] = {
+	// 					isConfigured: true,
+						
+	// 					profileId: locals.authUser.profileId,
+						
+	// 					username: locals.authUser.username,
+	// 					avatar: locals.authUser.avatar,
+						
+	// 					accessToken: locals.authUser.accessToken,
+	// 					refreshToken: locals.authUser.refreshToken
+	// 				}
+					
+	// 				// console.log('[auth.confirm] - New user data:', userData );
+					
+	// 				locals.existingUser = new User.model(userData);
+					
+	// 				locals.existingUser.save(function(err) {
+	// 					if (err) {
+	// 						console.log('[auth.confirm] - Error saving new user.', err);
+	// 						console.log('------------------------------------------------------------');
+	// 						return next({ message: 'Sorry, there was an error processing your account, please try again.' });
+	// 					}
+	// 					console.log('[auth.confirm] - Saved new user.');
+	// 					console.log('------------------------------------------------------------');
+	// 					return next();
+	// 				});
+					
+	// 			}
+			
+	// 		},
+			
+	// 		// Session
+	// 		function() {
+	// 			if (req.user) {
+	// 				console.log('[auth.confirm] - Already signed in, skipping sign in.');
+	// 				console.log('------------------------------------------------------------');
+	// 				return res.redirect(req.cookies.target || '/me');
+	// 			}
+	// 			return doSignIn();
+	// 		}
+		
+	// 	], function(err) {
+	// 		if (err) {
+	// 			console.log('[auth.confirm] - Issue signing user in.', err);
+	// 			console.log('------------------------------------------------------------');
+	// 			req.flash('error', err.message || 'Sorry, there was an issue signing you in, please try again.');
+	// 			return res.redirect('/');
+	// 		}
+	// 	});
+	
+	// }
+	
+	// view.on('init', function(next) {
+	// 	return checkExisting(next);
+	// });
+	
+	// view.on('post', { action: 'confirm.details' }, function(next) {
+	// 	if (!locals.form['name.first'] || !locals.form['name.last'] || !locals.form.email) {
+	// 		req.flash('error', 'Please enter a name & email.');
+	// 		return next();
+	// 	}
+	// 	return checkAuth();
+	// });
+	
+	view.render('profile');
+	
+}
--- server.js
+++ server.js
@@ -1,45 +1,5 @@
 var keystone = require('keystone');
 var passport = require('passport');
-var Strategy = require('passport-github').Strategy;
-
-// Configure the Github strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Github API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new Strategy({
-    clientID: '4fad542442f230983dfa', // process.env.CLIENT_ID,
-    clientSecret: '64e33995a9caf57be861272d4931d60a678c42c2', // process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/login/github/return'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Github profile is supplied as the user
-    // record.  In a production-quality application, the Github profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
-
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
 
 keystone.init({
 
@@ -53,8 +13,10 @@ keystone.init({
     'view engine': 'jade',
 
     'auth': true,
+    'session': true,
+    'session store': 'mongo', // this might through some ES6 related errors. Make sure you have latest node version installed
     'user model': 'User',
-    'cookie secret': 'keystoneStarterSecret',
+    'cookie secret': process.env.COOKIE_SECRET || 'keystoneStarterSecret',
 
     'auto update': false
 
@@ -62,7 +24,9 @@ keystone.init({
 
 keystone.import('models');
 
-
+// passport config
+// keystone.pre('routes', passport.initialize());
+// keystone.pre('routes', passport.session());
 
 keystone.set('routes', require('./routes'));
 
